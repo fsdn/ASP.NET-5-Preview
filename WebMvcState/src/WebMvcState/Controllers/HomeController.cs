a39d9e073d7586f43ad9936244ad55861023ab64
alloter <1296696830@qq.com>
2015-10-12 16:20:25 +0800

    WebMvcState

--- WebMvcState/src/WebMvcState/Controllers/HomeController.cs
+++ WebMvcState/src/WebMvcState/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace WebMvcState.Controllers
     {
         public IActionResult Index()
         {
-            ViewData["H"] = "Hello World";
+            ViewData["H"] = "Hello World！！！";
             return View();
         }
 
